23d1d19e9561d1b1d6a1dc61555268cae4b391e7
Peter Higgins <pah@cenabo.com>
2018-01-17 10:38:16 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'peter@example.comm',
-    text: 'Hello!'
-  });
 });
 
 socket.on('disconnect', function () {
